196d369cd668e7fbcebfd7cf691058f6834dd5cd
冉娃娃· <274544338@qq.com>
2020-01-17 10:23:00 +0800

    feat(icon example): add the example page of icon component

---
+++ src/pages/icon.vue
@@ -0,0 +1,25 @@
+<!-- author: ranwawa -->
+<!-- since: 2020/1/17 -->
+<!-- desc: 图标 -->
+<!-- remark:  -->
+<template>
+  <view class="icon">
+    <uv-icon name="chat" />
+  </view>
+</template>
+<script>
+import uvIcon from '@/components/icon.vue';
+
+export default {
+  name: 'icon',
+  components: {
+    uvIcon,
+  },
+};
+</script>
+<style
+  lang="scss"
+  scoped
+>
+
+</style>
