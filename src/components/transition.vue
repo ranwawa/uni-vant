b7d21001b609805826e9807f4008dd879f56337a
ranwawa <274544338>
2020-02-15 22:45:32 +0800

    fix(comp of transition): copy vant-weapp's scipt fully

--- src/components/transition.vue
+++ src/components/transition.vue
@@ -32,62 +32,88 @@ export default {
     transition-timing-function: ease;
   }
 
-  $fades: (
-    'fade-enter': 0,
-    'fade-enter-to': 1,
-    'fade-leave': 1,
-    'fade-leave-to': 0
-  );
-
-  @each $name, $value in $fades {
-    #{$PREFIX}#{$name} {
-      opacity: $value;
+  #{$PREFIX}fade {
+    &-enter-active,
+    &-leave-active {
+      transition-property: opacity;
     }
+
+    &-enter,
+    &-leave-to {
+      opacity: 0;
+    }
+
+    &-up-enter-active,
+    &-up-leave-active,
+    &-down-enter-active,
+    &-down-leave-active,
+    &-left-enter-active,
+    &-left-leave-active,
+    &-right-enter-active,
+    &-right-leave-active {
+      transition-property: opacity, transform;
+    }
+
+    &-up-enter,
+    &-up-leave-to {
+      transform: translate3d(0, 100%, 0);
+      opacity: 0;
+    }
+
+    &-down-enter,
+    &-down-leave-to {
+      transform: translate3d(0, -100%, 0);
+      opacity: 0;
+    }
+
+    &-left-enter,
+    &-left-leave-to {
+      transform: translate3d(-100%, 0, 0);
+      opacity: 0;
+    }
+
+    &-right-enter,
+    &-right-leave-to {
+      transform: translate3d(100%, 0, 0);
+      opacity: 0;
+    }
+
   }
 
-  .uv-fade-up-enter,
-  .uv-fade-up-leave-to {
-    transform: translate3d(0, 100%, 0);
-    opacity: 0;
-  }
 
-  .uv-fade-down-enter,
-  .uv-fade-down-leave-to {
-    transform: translate3d(0, -100%, 0);
-    opacity: 0;
-  }
+  #{$PREFIX}slide {
 
-  .uv-fade-left-enter,
-  .uv-fade-left-leave-to {
-    transform: translate3d(-100%, 0, 0);
-    opacity: 0;
-  }
+    &-up-enter-active,
+    &-up-leave-active,
+    &-down-enter-active,
+    &-down-leave-active,
+    &-left-enter-active,
+    &-left-leave-active,
+    &-right-enter-active,
+    &-right-leave-active {
+      transition-property: transform;
+    }
 
-  .uv-fade-right-enter,
-  .uv-fade-right-leave-to {
-    transform: translate3d(100%, 0, 0);
-    opacity: 0;
-  }
+    &-up-enter,
+    &-up-leave-to {
+      transform: translate3d(0, 100%, 0);
+    }
 
+    &-down-enter,
+    &-down-leave-to {
+      transform: translate3d(0, -100%, 0);
+    }
 
-  .uv-slide-up-enter,
-  .uv-slide-up-leave-to {
-    transform: translate3d(0, 100%, 0);
-  }
+    &-left-enter,
+    &-left-leave-to {
+      transform: translate3d(-100%, 0, 0);
+    }
 
-  .uv-slide-down-enter,
-  .uv-slide-down-leave-to {
-    transform: translate3d(0, -100%, 0);
-  }
+    &-right-enter,
+    &-right-leave-to {
+      transform: translate3d(100%, 0, 0);
+    }
 
-  .uv-slide-left-enter,
-  .uv-slide-left-leave-to {
-    transform: translate3d(-100%, 0, 0);
-  }
-
-  .uv-slide-right-enter,
-  .uv-slide-right-leave-to {
-    transform: translate3d(100%, 0, 0);
   }
 
 </style>
