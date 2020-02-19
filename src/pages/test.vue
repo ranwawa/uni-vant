c36b0659389f76013e0c95c5bb903deddbdc7ad7
冉娃娃· <274544338@qq.com>
2020-02-19 17:28:41 +0800

    feat(test): init

---
+++ src/pages/test.vue
@@ -0,0 +1,25 @@
+<!-- author: ranwawa -->
+<!-- since: 2020/2/19 -->
+<!-- desc:  -->
+<!-- remark:  -->
+<template>
+  <view class="container">
+    <uv-button color="red" plain>test</uv-button>
+  </view>
+</template>
+<script>
+import uvButton from '@/components/button';
+
+export default {
+  name: 'test',
+  components: {
+    uvButton,
+  },
+};
+</script>
+<style
+  lang="scss"
+  scoped
+>
+
+</style>
