49daca070c1cc64eb126c16c2f223d0b4b7a703e
冉娃娃· <274544338@qq.com>
2020-02-24 15:55:00 +0800

    feat(comp of result): init

---
+++ src/pages/result.vue
@@ -0,0 +1,27 @@
+<!-- author: ranwawa -->
+<!-- since: 2020/2/24 -->
+<!-- desc:  -->
+<!-- remark:  -->
+<template>
+  <view class="uv-doc">
+    <uv-section title="基础用法">
+      <uv-result />
+    </uv-section>
+  </view>
+</template>
+<script>
+import uvResult from '@/components/result.vue';
+
+export default {
+  name: 'result',
+  components: {
+    uvResult,
+  },
+};
+</script>
+<style
+  lang="scss"
+  scoped
+>
+
+</style>
