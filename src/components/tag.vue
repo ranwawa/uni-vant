6b347858b9065c461fa32084575bb2991062d6b8
ranwawa <274544338>
2020-02-25 23:05:48 +0800

    feat(comp of tag): init

---
+++ src/components/tag.vue
@@ -0,0 +1,30 @@
+<template>
+  <view
+    class="uv-tag"
+    :id="customId"
+  >
+  </view>
+</template>
+
+<script>
+import { baseMixin, bem } from './utils/index';
+
+export default {
+  name: 'uv-tag',
+  mixins: [baseMixin],
+  props: {},
+};
+</script>
+
+<style
+  scoped
+  lang="scss"
+>
+  @import './sass/_index.scss';
+
+  $comp: #{$PREFIX}tag;
+
+  #{$comp} {
+
+  }
+</style>
