121b3a59d939aebf6e0958b82a1d361d32851c0c
ranwawa <274544338>
2020-03-21 11:58:36 +0800

    refactor(build): npm directory

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,8 +1,8 @@
 const tasks = arr => arr.join(" && ");
 module.exports = {
   hooks: {
-    "pre-commit": "pretty-quick --staged",
-    "pre-push": tasks([
+    // "pre-commit": "pretty-quick --staged",
+    "pre-commit": tasks([
       "rm -rf npm/lib",
       "cp -rf src/components/. lib",
       "cd npm",
