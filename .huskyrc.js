902106e06218afc6c75a13f5e897d4eb4520641f
ranwawa <274544338>
2020-03-21 09:43:43 +0800

    refactor(sass): variables default

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,11 +1,12 @@
-const tasks = arr => arr.join(' && ');
+const tasks = arr => arr.join(" && ");
 module.exports = {
-  'hooks': {
-    'pre-push': tasks([
-      'rm -rf lib',
-      'cp -rf examples/src/components/. lib',
-      'npm version patch',
-      'npm publish',
-    ]),
-  },
+  hooks: {
+    "pre-commit": "pretty-quick --staged",
+    "pre-push": tasks([
+      "rm -rf lib",
+      "cp -rf examples/src/components/. lib",
+      "npm version patch",
+      "npm publish"
+    ])
+  }
 };
