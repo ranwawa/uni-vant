a48af15626277d2475ad5486025d54936fba6f80
冉娃娃 <274544338@qq.com>
2020-06-26 19:35:28 +0800

    feat(Price.vue): 完善价格组件
    可自定义尺寸,颜色及小数点位数

--- src/components/price.vue
+++ src/components/price.vue
@@ -4,41 +4,53 @@
 <!-- since: 2020/3/24 -->
 <!-- desc: 价格 -->
 <template>
-  <view class="uv-price" @click="$emit('click', amount)">
+  <view
+    class="uv-price"
+    :class="customClass"
+    :style="computedCustomStyle"
+    @click="$emit('click', amount)"
+  >
     <text class="uv-price__unit">
       {{ unit }}
     </text>
     <text class="uv-price__integer">
       {{ inte }}
     </text>
-    <text class="uv-price__decimal"> .{{ deci }} </text>
+    <text class="uv-price__decimal"> .{{ deci }}</text>
   </view>
 </template>
 <script>
+import { baseMixin, bem } from "./utils";
+
 export default {
-  name: 'UvPrice',
+  name: "UvPrice",
+  mixins: [baseMixin],
   props: {
     unit: {
       type: String,
-      default: '￥',
+      default: "￥",
     },
     amount: {
       type: [String, Number],
-      default: '0.00',
+      default: "0.00",
+    },
+    decimal: {
+      type: Number,
+      default: 2,
     },
     size: {
       type: [String, Number],
-      default: 18,
+      default: 14,
     },
     color: {
       type: String,
-      default: '',
+      default: "",
     },
   },
   data() {
     return {
       inte: 0,
-      deci: '00',
+      deci: "00",
     };
   },
   watch: {
@@ -49,6 +61,20 @@ export default {
       this.handleAmount(newValue);
     },
   },
+  computed: {
+    computedCustomStyle() {
+      const { size } = this;
+      const type = typeof size;
+      let fontSize;
+      if (type === "string") {
+        fontSize = `${size}px`;
+      } else {
+        fontSize = /[a-zA-Z]{2,3}/.test(size) ? size : `${size}px`;
+      }
+      const color = this.color ? `color: ${this.color};` : "";
+      return `${this.customStyle}; font-size: ${fontSize}; ${color}`;
+    },
+  },
   mounted() {
     this.handleAmount(this.amount);
   },
@@ -56,16 +82,18 @@ export default {
     handleAmount(newValue) {
       let num = +newValue;
       // eslint-disable-next-line no-self-compare
-      num = num !== num ? '0.00' : num.toFixed(2);
-      [this.inte, this.deci] = num.split('.');
+      num !== num && (num = 0);
+      let deci = num % 1;
+      this.inte = num - deci;
+      deci = Math.trunc(deci * 10 ** this.decimal);
+      this.deci = deci;
     },
   },
 };
 </script>
 <style lang="scss" scoped>
-$price-color: #f05b28 !default;
-$price-size: 24rpx;
-$price-unit-margin: 2rpx;
+@import "./sass/variables";
+
 .uv-price {
   display: inline-block;
   color: $price-color;
@@ -73,10 +101,12 @@ $price-unit-margin: 2rpx;
   font-weight: bold;
   line-height: 1;
   vertical-align: baseline;
+
   &__unit {
     display: inline-block;
     margin-right: $price-unit-margin;
   }
+
   &__integer {
     font-size: 2em;
   }
---
+++ src/pages/price.vue
@@ -0,0 +1,37 @@
+<!--
+ * @file 价格组件示例页面
+ * @version 0.0.1
+ * @author 冉娃娃 <274544338@qq.com>
+ * @since 2020/6/26 18:51
+-->
+<template>
+  <view class="uv-doc">
+    <uv-platform html weixin />
+    <uv-section title="基础用法">
+      <uv-price></uv-price>
+      <uv-price amount="168"></uv-price>
+      <uv-price amount="168.8888"></uv-price>
+    </uv-section>
+    <uv-section title="尺寸">
+      <uv-price amount="168.8888" size="22"></uv-price>
+    </uv-section>
+    <uv-section title="颜色">
+      <uv-price amount="168.8888" color="#000"></uv-price>
+    </uv-section>
+    <uv-section title="小数点位数">
+      <uv-price amount="168.8888" :decimal="4"></uv-price>
+    </uv-section>
+  </view>
+</template>
+
+<script>
+import UvPrice from "../components/price.vue";
+
+export default {
+  components: {
+    UvPrice,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
