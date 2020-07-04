cbe4dd42d2a778bd60a681c1bc5c31aea73c8d38
冉娃娃 <274544338@qq.com>
2020-07-04 19:21:01 +0800

    feat(swiper-dot.vue): nav模式下文字显示逻辑调整

--- src/components/swiper-dot.vue
+++ src/components/swiper-dot.vue
@@ -8,66 +8,99 @@
     <slot />
     <view
       v-if="mode === 'default'"
-      :style="{'bottom':dots.bottom + 'rpx'}"
+      :style="{ bottom: dots.bottom + 'rpx' }"
       class="uv-swiper__dots-box"
     >
       <view
-        v-for="(item,index) in info"
+        v-for="(item, index) in info"
         :key="index"
         :style="{
-        'width': (index === current? dots.width*2:dots.width ) + 'rpx','height':dots.width/3 +'rpx' ,'background-color':index !== current?dots.backgroundColor:dots.selectedBackgroundColor,'border-radius':'0'}"
+          width: (index === current ? dots.width * 2 : dots.width) + 'rpx',
+          height: dots.width / 3 + 'rpx',
+          'background-color':
+            index !== current
+              ? dots.backgroundColor
+              : dots.selectedBackgroundColor,
+          'border-radius': '0',
+        }"
         class="uv-swiper__dots-item uv-swiper__dots-bar"
       />
     </view>
     <view
       v-else-if="mode === 'dot'"
-      :style="{'bottom':dots.bottom + 'rpx'}"
+      :style="{ bottom: dots.bottom + 'rpx' }"
       class="uv-swiper__dots-box"
     >
       <view
-        v-for="(item,index) in info"
+        v-for="(item, index) in info"
         :key="index"
         :style="{
-        'width': dots.width + 'rpx','height':dots.height +'rpx' ,'background-color':index !== current?dots.backgroundColor:dots.selectedBackgroundColor,'border':index !==current ? dots.border:dots.selectedBorder}"
+          width: dots.width + 'rpx',
+          height: dots.height + 'rpx',
+          'background-color':
+            index !== current
+              ? dots.backgroundColor
+              : dots.selectedBackgroundColor,
+          border: index !== current ? dots.border : dots.selectedBorder,
+        }"
         class="uv-swiper__dots-item"
       />
     </view>
     <view
       v-else-if="mode === 'round'"
-      :style="{'bottom':dots.bottom + 'rpx'}"
+      :style="{ bottom: dots.bottom + 'rpx' }"
       class="uv-swiper__dots-box"
     >
       <view
-        v-for="(item,index) in info"
+        v-for="(item, index) in info"
         :key="index"
-        :class="[index === current&&'uv-swiper__dots-long']"
-        :style="{'width':(index === current? dots.width*3:dots.width ) + 'rpx','height':dots.height +'rpx' ,'background-color':index !== current?dots.backgroundColor:dots.selectedBackgroundColor,'border':index !==current ? dots.border:dots.selectedBorder}"
-        class="uv-swiper__dots-item "
+        :class="[index === current && 'uv-swiper__dots-long']"
+        :style="{
+          width: (index === current ? dots.width * 3 : dots.width) + 'rpx',
+          height: dots.height + 'rpx',
+          'background-color':
+            index !== current
+              ? dots.backgroundColor
+              : dots.selectedBackgroundColor,
+          border: index !== current ? dots.border : dots.selectedBorder,
+        }"
+        class="uv-swiper__dots-item"
       />
     </view>
     <view
       v-else-if="mode === 'nav'"
-      :style="{'background-color':dotsStyles.backgroundColor,'bottom':'0'}"
+      :style="{ 'background-color': dotsStyles.backgroundColor, bottom: '0' }"
       class="uv-swiper__dots-box uv-swiper__dots-nav"
     >
       <text
-        :style="{'color':dotsStyles.color}"
+        :style="{ color: dotsStyles.color }"
         class="uv-swiper__dots-nav-item"
-      >{{ (current+1)+'/'+info.length +' ' +info[current][field] }}
+      >
+        {{ current + 1 + "/" + info.length }}
+        {{ info[current][field] ? " " + info[current][field] : "" }}
       </text>
     </view>
     <view
       v-else-if="mode === 'indexes'"
-      :style="{'bottom':dots.bottom + 'rpx'}"
+      :style="{ bottom: dots.bottom + 'rpx' }"
       class="uv-swiper__dots-box"
     >
       <view
-        v-for="(item,index) in info"
+        v-for="(item, index) in info"
         :key="index"
-        :style="{ 'width':dots.width + 'rpx','height':dots.height +'rpx' ,'color':index === current?dots.selectedColor:dots.color,'background-color':index !== current?dots.backgroundColor:dots.selectedBackgroundColor,'border':index !==current ? dots.border:dots.selectedBorder}"
+        :style="{
+          width: dots.width + 'rpx',
+          height: dots.height + 'rpx',
+          color: index === current ? dots.selectedColor : dots.color,
+          'background-color':
+            index !== current
+              ? dots.backgroundColor
+              : dots.selectedBackgroundColor,
+          border: index !== current ? dots.border : dots.selectedBorder,
+        }"
         class="uv-swiper__dots-item uv-swiper__dots-indexes"
       >
-        <text class="uv-swiper__dots-indexes-text">{{ index+1 }}</text>
+        <text class="uv-swiper__dots-indexes-text">{{ index + 1 }}</text>
       </view>
     </view>
   </view>
@@ -90,7 +123,7 @@
  * @event {Function} clickItem 组件触发点击事件时触发，e={currentIndex}
  */
 export default {
-  name: 'UvSwiperDot',
+  name: "UvSwiperDot",
   props: {
     info: {
       type: Array,
@@ -111,15 +144,14 @@ export default {
     // 类型 ：default(默认) indexes long nav
     mode: {
       type: String,
-      default: 'default',
-      validate: (value) => [
-        'default', 'round', 'nav', 'indexes',
-      ].includes(value),
+      default: "default",
+      validate: (value) =>
+        ["default", "round", "nav", "indexes"].includes(value),
     },
     // 只在 nav 模式下生效，变量名称
     field: {
       type: String,
-      default: '',
+      default: "",
     },
   },
   data() {
@@ -128,11 +160,11 @@ export default {
         width: 16,
         height: 16,
         bottom: 20,
-        color: '#fff',
-        backgroundColor: 'rgba(0, 0, 0, .3)',
-        border: '1px rgba(0, 0, 0, .3) solid',
-        selectedBackgroundColor: '#333',
-        selectedBorder: '1px rgba(0, 0, 0, .9) solid',
+        color: "#fff",
+        backgroundColor: "rgba(0, 0, 0, .3)",
+        border: "1px rgba(0, 0, 0, .3) solid",
+        selectedBackgroundColor: "#333",
+        selectedBorder: "1px rgba(0, 0, 0, .9) solid",
       },
     };
   },
@@ -141,7 +173,7 @@ export default {
       this.dots = Object.assign(this.dots, this.dotsStyles);
     },
     mode(newVal) {
-      if (newVal === 'indexes') {
+      if (newVal === "indexes") {
         this.dots.width = 40;
         this.dots.height = 40;
       } else {
@@ -151,7 +183,7 @@ export default {
     },
   },
   created() {
-    if (this.mode === 'indexes') {
+    if (this.mode === "indexes") {
       this.dots.width = 40;
       this.dots.height = 40;
     }
@@ -160,94 +192,91 @@ export default {
 };
 </script>
 
-<style
-  lang="scss"
-  scoped
->
-  @import "./sass/_index.scss";
+<style lang="scss" scoped>
+@import "./sass/_index.scss";
 
-  $comp: #{$PREFIX}swiper;
+$comp: #{$PREFIX}swiper;
 
-  #{$comp} {
-    font-size: $font-size-md;
-    &__warp {
-      /* #ifndef APP-NVUE */
-      position: relative;
-      /* #endif */
-      display: flex;
-      overflow: hidden;
-      flex: 1;
-      flex-direction: column;
-    }
+#{$comp} {
+  font-size: $font-size-md;
+  &__warp {
+    /* #ifndef APP-NVUE */
+    position: relative;
+    /* #endif */
+    display: flex;
+    overflow: hidden;
+    flex: 1;
+    flex-direction: column;
+  }
 
-    &__dots-box {
-      position: absolute;
-      right: 0;
-      bottom: $swiper-dot-box-bottom;
-      left: 0;
-      /* #ifndef APP-NVUE */
-      display: flex;
-      /* #endif */
-      align-items: center;
-      flex: 1;
-      flex-direction: row;
-      justify-content: center;
-    }
+  &__dots-box {
+    position: absolute;
+    right: 0;
+    bottom: $swiper-dot-box-bottom;
+    left: 0;
+    /* #ifndef APP-NVUE */
+    display: flex;
+    /* #endif */
+    align-items: center;
+    flex: 1;
+    flex-direction: row;
+    justify-content: center;
+  }
 
-    &__dots-item {
-      width: $swiper-dot-item-width;
-      margin-left: $swiper-dot-item-margin;
-      border-radius: $swiper-dot-item-radius;
-      background-color: rgba(0, 0, 0, 0.4);;
-      // transition: width 0.2s linear;  不要取消注释，不然会不能变色
-    }
+  &__dots-item {
+    width: $swiper-dot-item-width;
+    margin-left: $swiper-dot-item-margin;
+    border-radius: $swiper-dot-item-radius;
+    background-color: rgba(0, 0, 0, 0.4);
+    // transition: width 0.2s linear;  不要取消注释，不然会不能变色
+  }
 
-    &__dots-item:first-child {
-      margin: 0;
-    }
+  &__dots-item:first-child {
+    margin: 0;
+  }
 
-    &__dots-default {
-      border-radius: $swiper-dot-item-radius;
-    }
+  &__dots-default {
+    border-radius: $swiper-dot-item-radius;
+  }
 
-    &__dots-long {
-      border-radius: $swiper-dot-default-radius;
-    }
+  &__dots-long {
+    border-radius: $swiper-dot-default-radius;
+  }
 
-    &__dots-bar {
-      border-radius: $swiper-dot-default-radius;
-    }
+  &__dots-bar {
+    border-radius: $swiper-dot-default-radius;
+  }
 
-    &__dots-nav {
-      bottom: 0;
-      display: flex;
-      /* #ifndef APP-NVUE */
-      align-items: center;
-      /* #endif */
-      flex: 1;
-      flex-direction: row;
-      justify-content: flex-start;
-      height: $swiper-dot-nav-height;
-      background-color: rgba(0, 0, 0, 0.2);
-    }
+  &__dots-nav {
+    bottom: 0;
+    display: flex;
+    /* #ifndef APP-NVUE */
+    align-items: center;
+    /* #endif */
+    flex: 1;
+    flex-direction: row;
+    justify-content: flex-start;
+    height: $swiper-dot-nav-height;
+    background-color: rgba(0, 0, 0, 0.2);
+  }
 
-    &__dots-nav-item {
-      margin: 0 $swiper-dot-item-margin;
+  &__dots-nav-item {
+    margin: 0 $swiper-dot-item-margin;
+    color: #fff;
+    font-size: $swiper-dot-item-size;
+  }
+
+  &__dots-indexes {
+    /* #ifndef APP-NVUE */
+    display: flex;
+    /* #endif */
+    align-items: center;
+    justify-content: center;
+
+    &-text {
       color: #fff;
-      font-size: $swiper-dot-item-size;
-    }
-
-    &__dots-indexes {
-      /* #ifndef APP-NVUE */
-      display: flex;
-      /* #endif */
-      align-items: center;
-      justify-content: center;
-
-      &-text {
-        color: #fff;
-        font-size: $swiper-dot-text-size;
-      }
+      font-size: $swiper-dot-text-size;
     }
   }
+}
 </style>
